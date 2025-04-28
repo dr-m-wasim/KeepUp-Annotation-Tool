5dfd6366db57666de7d990a24b76f1654b48c47a
MirBytes <mirmutabassimali@gmail.com>
2025-04-28 13:19:40 +0500

    Database Updated

--- KAT/KATAPP/models.py
+++ KAT/KATAPP/models.py
@@ -102,6 +102,9 @@ class Comments(models.Model):
     likescount_on_comment = models.FloatField(db_column='likescount on comment', blank=True, null=True)  # Field renamed to remove unsuitable characters.
     comment_label = models.TextField(db_column='comment label', blank=True, null=True)  # Field renamed to remove unsuitable characters.
     label = models.TextField(db_column='Label', blank=True, null=True)  # Field name made lowercase.
+    annotatorOne_comment_label = models.TextField(db_column='annotatorOne_comment_label', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    annotatorTwo_comment_label = models.TextField(db_column='annotatorTwo_comment_label', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    annotatorThree_comment_label = models.TextField(db_column='annotatorThree_comment_label', blank=True, null=True)  # Field renamed to remove unsuitable characters.
 
     class Meta:
         managed = False
